8740c26ecf713bb13e6babcfbc55f7753fd022a5
Michée NKUSU <nkusumichee1367@gmail.com>
2022-03-02 10:29:35 +0100

    fix bug

--- src/Security/KeycloakAuthenticator.php
+++ src/Security/KeycloakAuthenticator.php
@@ -2,15 +2,28 @@
 
 namespace Mink67\Security\Security;
 
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use KnpU\OAuth2ClientBundle\Client\Provider\KeycloakClient;
+use Stevenmaguire\OAuth2\Client\Provider\Keycloak;
+use KnpU\OAuth2ClientBundle\Security\User\OAuthUserProvider;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use \Mink67\Security\Converters\AccessTokenToUser;
 
@@ -43,11 +56,14 @@ class KeycloakAuthenticator extends AbstractAuthenticator {
      */
     public function authenticate(Request $request): Passport
     {
+        $identifier = "" ;
         $bearerToken = $request->headers->get("Authorization");
-        $identifier = "";
-        if (! is_null($bearerToken)) {
+        if (!is_null($bearerToken)) {
+            
             $identifier = str_replace("Bearer ", "", $request->headers->get("Authorization"));
         }
+
+        //dd($request->headers->get("Authorization"));
 
         $userConverter = $this->getUserConverter();
 
@@ -57,11 +73,8 @@ class KeycloakAuthenticator extends AbstractAuthenticator {
                 function (string $identifier) use($userConverter)
                 {
                     if (empty($identifier)) {
-
                         return null;
-
                     }
-                    
                     $accessToken = new AccessToken(["access_token" => $identifier, "token_type" => "bearer"]);
                     return $userConverter($accessToken);
                 }
