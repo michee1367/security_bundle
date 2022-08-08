23ba22b0923a573d143384d4d9ac6be56a5e8724
Michée NKUSU <nkusumichee1367@gmail.com>
2022-08-08 13:42:21 +0100

    fix bug

--- src/Security/KeycloakAuthenticator.php
+++ src/Security/KeycloakAuthenticator.php
@@ -99,8 +99,8 @@ class KeycloakAuthenticator extends AbstractAuthenticator {
         $data = [
             // you may want to customize or obfuscate the message first
             //'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
-            'message'=>'Authentification échouée'
-            'detail'=> $exception->getMessageKey()
+            'message'=>'Authentification échouée',
+            'detail'=> $exception->getMessageData()
             // or to translate this message
             // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
         ];
